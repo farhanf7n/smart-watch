3fa74c16e736bf0354cd5176aa294fa72cfa222a
farhanf7n <farhanf7n@gmail.com>
2021-11-13 20:12:13 +0500

    Added Time Function

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let scolor = document.getElementById('secondColor')
 let tcolor = document.getElementById('thirdColor')
 let focolor = document.getElementById('fourColor')
 let ficolor = document.getElementById('fiveColor')
+let tShow = document.getElementById('showTime')
+let showBtn = document.getElementById('timeBtn')
+
+
 
 let chImg = document.getElementById('chng-img')
 
@@ -25,3 +29,21 @@ focolor.addEventListener("click", function(){
 ficolor.addEventListener("click", function(){
     chImg.src = './images/skin-watch.png'
 })
+
+showBtn.addEventListener("click", function () {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
+
+    function startTime() {
+        var today = new Date(),
+        h = checkTime(today.getHours()),
+        m = checkTime(today.getMinutes()),
+        s = checkTime(today.getSeconds());
+        tShow.textContent = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+});
