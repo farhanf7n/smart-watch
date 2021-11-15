3728f2abb08d087adab91b91140266de028a8098
farhanf7n <farhanf7n@gmail.com>
2021-11-15 11:46:32 +0500

    Completed

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ let focolor = document.getElementById('fourColor')
 let ficolor = document.getElementById('fiveColor')
 let tShow = document.getElementById('showTime')
 let showBtn = document.getElementById('timeBtn')
-
-
+let heartBtn = document.getElementById('hrBtn')
+let heartShow = document.getElementById('hr-part')
+let timeShow = document.getElementById('time-part')
+let aodBtn = document.getElementById('aod-btn')
+let aodShow = document.getElementById('aod-part')
 
 let chImg = document.getElementById('chng-img')
 
@@ -31,6 +34,9 @@ ficolor.addEventListener("click", function(){
 })
 
 showBtn.addEventListener("click", function () {
+    heartShow.style.display = 'none';
+    aodShow.style.display = 'none';
+    timeShow.style.display = 'block';
     function checkTime(i) {
         return (i < 10) ? "0" + i : i;
     }
@@ -47,3 +53,15 @@ showBtn.addEventListener("click", function () {
     }
     startTime();
 });
+
+heartBtn.addEventListener("click", function(){
+    timeShow.style.display = 'none';
+    aodShow.style.display = 'none';
+    heartShow.style.display = 'block';
+})
+
+aodBtn.addEventListener("click", function() {
+    heartShow.style.display = 'none';
+    timeShow.style.display = 'none';
+    aodShow.style.display = 'block';
+})
